23a108beaa81a68c670972268f56e69b4750b581
om1cael <188113439+om1cael@users.noreply.github.com>
2026-05-03 13:52:13 -0300

    chore: add basic sql schema

---
+++ schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(24) NOT NULL UNIQUE,
+    password VARCHAR(72) NOT NULL,
+    level INT DEFAULT 0,
+    money INT DEFAULT 500,
+    pos_x FLOAT,
+    pos_y FLOAT,
+    pos_z FLOAT,
+) ENGINE=InnoDB;
