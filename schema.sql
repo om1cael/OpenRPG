b6eb26b7018cec66e86e90bc3386992422bf3765
om1cael <188113439+om1cael@users.noreply.github.com>
2026-05-03 17:11:00 -0300

    feat: save skin and job

--- schema.sql
+++ schema.sql
@@ -4,8 +4,10 @@ CREATE TABLE IF NOT EXISTS users (
     password VARCHAR(72) NOT NULL,
     level INT DEFAULT 0,
     money INT DEFAULT 500,
+    skin INT DEFAULT 221,
+    job INT DEFAULT 0,
     pos_x FLOAT,
     pos_y FLOAT,
     pos_z FLOAT,
-    p_interior INT DEFAULT 0
+    interior INT DEFAULT 0
 ) ENGINE=InnoDB;
